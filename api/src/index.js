e4e255e5f418a471016a7f6102e3d5ee1d159b60
spencermendoza <spencerjmendoza@gmail.com>
2021-07-20 11:07:48 -0400

    added client/build

--- api/src/index.js
+++ api/src/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
+  app.use(express.static('client/build'));
 }
 // app.use(express.static('build'));
 
