56eb930673f482dc541f9c6e00d847944a52c9f6
spencermendoza <spencerjmendoza@gmail.com>
2021-07-20 11:15:12 -0400

    just trying to get the express app working again

--- api/src/index.js
+++ api/src/index.js
@@ -20,9 +20,9 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('src/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('src/build'));
+// }
 // app.use(express.static('build'));
 
   // Add headers
